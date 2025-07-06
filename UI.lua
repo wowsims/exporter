b981cde082238366a00fa5606fa2ff82c6567ce9
rosen.rusinov <rosen.rusinov@scopely.com>
2025-07-06 15:25:16 +0200

    add local saves support to be used by external apps

--- UI.lua
+++ UI.lua
@@ -45,7 +45,7 @@ function UI:CreateMainWindow(classIsSupported, simLink)
     frame:SetCallback("OnClose", OnClose)
     frame:SetTitle("WowSimsExporter " .. Env.VERSION .. "")
     frame:SetStatusText("Click 'Generate Data' to generate exportable data")
-    frame:SetLayout("Flow")
+    frame:SetLayout("Fill")
 
     -- Add the frame as a global variable under the name `WowSimsExporter`
     _G["WowSimsExporter"] = frame.frame
@@ -55,23 +55,33 @@ function UI:CreateMainWindow(classIsSupported, simLink)
 
     _frame = frame
 
-    local icon = AceGUI:Create("Icon")
-    icon:SetImage("Interface\\AddOns\\wowsimsexporter\\Skins\\wowsims.tga")
-    icon:SetImageSize(32, 32)
-    icon:SetFullWidth(true)
-    frame:AddChild(icon)
+    local tabGroup = AceGUI:Create("TabGroup")
+    tabGroup:SetFullWidth(true)
+    tabGroup:SetFullHeight(true)
+    tabGroup:SetLayout("Flow")
+    tabGroup:SetTabs({
+        { text = "Main", value = "main" },
+        { text = "Saved Data", value = "saveddata" },
+    })
 
-    local label = AceGUI:Create("Label")
-    label:SetFullWidth(true)
-    frame:AddChild(label)
+    local function CreateMainTab(container)
+        local icon = AceGUI:Create("Icon")
+        icon:SetImage("Interface\\AddOns\\wowsimsexporter\\Skins\\wowsims.tga")
+        icon:SetImageSize(32, 32)
+        icon:SetFullWidth(true)
+        container:AddChild(icon)
 
-    if not classIsSupported then
-        label:SetText("Your characters class is currently unsupported. The supported classes are currently:\n" ..
-            table.concat(Env.supportedClasses, "\n"))
-        return
-    end
+        local label = AceGUI:Create("Label")
+        label:SetFullWidth(true)
+        container:AddChild(label)
 
-    label:SetText([[
+        if not classIsSupported then
+            label:SetText("Your characters class is currently unsupported. The supported classes are currently:\n" ..
+                table.concat(Env.supportedClasses, "\n"))
+            return
+        end
+
+        label:SetText([[
 
 To upload your character to the simuator, click on the url below that leads to the simuator website.
 
@@ -80,47 +90,159 @@ into the provided box and click "Import"
 
 ]])
 
-    if simLink then
-        local ilabel = AceGUI:Create("InteractiveLabel")
-        ilabel:SetText("Click to copy: " .. simLink .. "\r\n")
-        ilabel:SetFullWidth(true)
-        ilabel:SetCallback("OnClick", function()
-            CreateCopyDialog(simLink)
+        if simLink then
+            local ilabel = AceGUI:Create("InteractiveLabel")
+            ilabel:SetText("Click to copy: " .. simLink .. "\r\n")
+            ilabel:SetFullWidth(true)
+            ilabel:SetCallback("OnClick", function()
+                CreateCopyDialog(simLink)
+            end)
+            container:AddChild(ilabel)
+        end
+
+        local button = AceGUI:Create("Button")
+        button:SetText("Generate Data (Equipped Only)")
+        button:SetWidth(300)
+        button:SetCallback("OnClick", function()
+            if _outputGenerator then
+                UI:SetOutput(_outputGenerator())
+            end
         end)
-        frame:AddChild(ilabel)
+        container:AddChild(button)
+
+        local extraButton = AceGUI:Create("Button")
+        extraButton:SetText("Batch: Export Bag Items")
+        extraButton:SetWidth(300)
+        extraButton:SetCallback("OnClick", function()
+            if _outputGeneratorBags then
+                UI:SetOutput(_outputGeneratorBags())
+            end
+        end)
+        container:AddChild(extraButton)
+
+        local jsonbox = AceGUI:Create("MultiLineEditBox")
+        jsonbox:SetLabel("Copy and paste into the websites importer!")
+        jsonbox:SetFullWidth(true)
+        jsonbox:SetFullHeight(true)
+        jsonbox:DisableButton(true)
+        if jsonbox.editBox then
+            jsonbox.editBox:SetScript("OnEscapePressed", function(self)
+                OnClose(frame)
+            end)
+        end
+        container:AddChild(jsonbox)
+
+        _jsonbox = jsonbox
     end
 
-    local button = AceGUI:Create("Button")
-    button:SetText("Generate Data (Equipped Only)")
-    button:SetWidth(300)
-    button:SetCallback("OnClick", function()
-        if _outputGenerator then
-            UI:SetOutput(_outputGenerator())
+    local function CreateSavedDataTab(container)
+        local scrollFrame = AceGUI:Create("ScrollFrame")
+        scrollFrame:SetFullWidth(true)
+        scrollFrame:SetFullHeight(true)
+        scrollFrame:SetLayout("Flow")
+        container:AddChild(scrollFrame)
+
+        local label = AceGUI:Create("Label")
+        label:SetText("Saved Character Data Management")
+        label:SetFontObject(GameFontNormalLarge)
+        label:SetFullWidth(true)
+        scrollFrame:AddChild(label)
+
+        local autoSaveGroup = AceGUI:Create("InlineGroup")
+        autoSaveGroup:SetTitle("Auto-Save Settings")
+        autoSaveGroup:SetFullWidth(true)
+        autoSaveGroup:SetLayout("Flow")
+        scrollFrame:AddChild(autoSaveGroup)
+
+        local autoSaveCheckbox = AceGUI:Create("CheckBox")
+        autoSaveCheckbox:SetLabel("Enable Auto-Save")
+        autoSaveCheckbox:SetValue(Env.SavedDataManager.db and Env.SavedDataManager.db.profile.autoSaveEnabled or false)
+        autoSaveCheckbox:SetCallback("OnValueChanged", function(widget, event, value)
+            if Env.SavedDataManager.db then
+                Env.SavedDataManager.db.profile.autoSaveEnabled = value
+                local status = value and "enabled" or "disabled"
+                DEFAULT_CHAT_FRAME:AddMessage("[WowSimsExporter] Auto-save " .. status .. ".")
+            end
+        end)
+        autoSaveGroup:AddChild(autoSaveCheckbox)
+
+        local messagesCheckbox = AceGUI:Create("CheckBox")
+        messagesCheckbox:SetLabel("Show Auto-Save Messages")
+        messagesCheckbox:SetValue(Env.SavedDataManager.db and Env.SavedDataManager.db.profile.showAutoSaveMessages or false)
+        messagesCheckbox:SetCallback("OnValueChanged", function(widget, event, value)
+            if Env.SavedDataManager.db then
+                Env.SavedDataManager.db.profile.showAutoSaveMessages = value
+                local status = value and "enabled" or "disabled"
+                DEFAULT_CHAT_FRAME:AddMessage("[WowSimsExporter] Auto-save messages " .. status .. ".")
+            end
+        end)
+        autoSaveGroup:AddChild(messagesCheckbox)
+
+        local savedCharsGroup = AceGUI:Create("InlineGroup")
+        savedCharsGroup:SetTitle("Saved Characters")
+        savedCharsGroup:SetFullWidth(true)
+        savedCharsGroup:SetLayout("Flow")
+        scrollFrame:AddChild(savedCharsGroup)
+
+        local function RefreshSavedCharsList()
+            savedCharsGroup:ReleaseChildren()
+            
+            local savedChars = Env.SavedDataManager:GetSavedCharacters()
+            
+            if #savedChars == 0 then
+                local noDataLabel = AceGUI:Create("Label")
+                noDataLabel:SetText("No saved characters found.")
+                noDataLabel:SetFullWidth(true)
+                savedCharsGroup:AddChild(noDataLabel)
+                return
+            end
+
+            for i, charData in ipairs(savedChars) do
+                local charGroup = AceGUI:Create("InlineGroup")
+                charGroup:SetTitle(charData.name)
+                charGroup:SetFullWidth(true)
+                charGroup:SetLayout("Flow")
+                savedCharsGroup:AddChild(charGroup)
+
+                local infoLabel = AceGUI:Create("Label")
+                infoLabel:SetText("Saved: " .. charData.dateString)
+                infoLabel:SetWidth(200)
+                charGroup:AddChild(infoLabel)
+
+                local exportButton = AceGUI:Create("Button")
+                exportButton:SetText("Export")
+                exportButton:SetWidth(80)
+                exportButton:SetCallback("OnClick", function()
+                    tabGroup:SelectTab("main")
+                    UI:SetOutput(charData.data)
+                end)
+                charGroup:AddChild(exportButton)
+
+                local deleteButton = AceGUI:Create("Button")
+                deleteButton:SetText("Delete")
+                deleteButton:SetWidth(80)
+                deleteButton:SetCallback("OnClick", function()
+                    Env.SavedDataManager:DeleteSavedCharacter(i)
+                    RefreshSavedCharsList()
+                end)
+                charGroup:AddChild(deleteButton)
+            end
+        end
+
+        RefreshSavedCharsList()
+    end
+
+    tabGroup:SetCallback("OnGroupSelected", function(self, event, group)
+        self:ReleaseChildren()
+        if group == "main" then
+            CreateMainTab(self)
+        elseif group == "saveddata" then
+            CreateSavedDataTab(self)
         end
     end)
-    frame:AddChild(button)
 
-    local extraButton = AceGUI:Create("Button")
-    extraButton:SetText("Batch: Export Bag Items")
-    extraButton:SetWidth(300)
-    extraButton:SetCallback("OnClick", function()
-        if _outputGeneratorBags then
-            UI:SetOutput(_outputGeneratorBags())
-        end
-    end)
-    frame:AddChild(extraButton)
-
-    local jsonbox = AceGUI:Create("MultiLineEditBox")
-    jsonbox:SetLabel("Copy and paste into the websites importer!")
-    jsonbox:SetFullWidth(true)
-    jsonbox:SetFullHeight(true)
-    jsonbox:DisableButton(true)
-    jsonbox.editBox:SetScript("OnEscapePressed", function(self)
-        OnClose(frame)
-    end)
-    frame:AddChild(jsonbox)
-
-    _jsonbox = jsonbox
+    frame:AddChild(tabGroup)
+    tabGroup:SelectTab("main")
 end
 
 ---Create a button on the character panel that will call the provided function
